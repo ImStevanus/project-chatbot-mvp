4d3bebaecaad40b64f49f20995763f9b5b43c7e1
Ahmad Faris Maulana <ahmadfarisaf099941@gmail.com>
2025-12-12 15:44:36 +0700

    update ai

--- main_app.py
+++ main_app.py
@@ -17,7 +17,7 @@ def ask_gemini(query):
         if "GOOGLE_API_KEY" in st.secrets:
             api_key = st.secrets["GOOGLE_API_KEY"]
             genai.configure(api_key=api_key)
-            model = genai.GenerativeModel('gemini-1.5-flash')
+            model = genai.GenerativeModel('gemini-2.0-flash')
             
             prompt = f"""
             Kamu adalah 'AI Course Advisor' untuk Universitas Bunda Mulia (UBM).
